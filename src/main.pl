70b8e9393cd7698017486afddb8b9304b7ebc078
Matheus-Adiel <matheuus.adiiel@gmail.com>
2025-09-07 14:17:53 -0300

    refactor: rules moved to their respective modules.

--- src/main.pl
+++ src/main.pl
@@ -1,22 +1,8 @@
-:- use_module(display_board).
-:- use_module(ansi_terminal).
-:- use_module(board).
+:- use_module(gameLoop).
 :- initialization(main).
 
 main :-
     Configs = configs{height: 9, width: 19, timer: 120},
     init_game(Configs),
-    movePointer(0, 11),
     halt.
-
-/*
-inicia um jogo com um conjunto de configurações (Configs).
-
-@predicate init_game(+Configs).
-
-@param Configs uma dict contendo as configurações do jogo.
-*/
-init_game(Configs) :-
-    createBoard(Configs, Board),
-    displayBoard(Board).
 
