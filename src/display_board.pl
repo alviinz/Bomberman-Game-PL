842a23bd3e925aff1633b89868e9aa6cabda9205
Matheus-Adiel <matheuus.adiiel@gmail.com>
2025-09-06 23:47:08 -0300

    feito a estrutura básica

---
+++ src/display_board.pl
@@ -0,0 +1,45 @@
+:- module(display_board, [displayPoint/2,
+                          displayPoints/2,
+                          displayBoard/1]).
+
+:- use_module(ansi_terminal).
+:- use_module(utils).
+
+/*
+escreve em uma coordenada da tela (Point) um símbolo (Symboll).
+
+@predicate displayPoint(+Point, +Symbol).
+
+@param Point  o ponto que contém as coordenadas X e Y para escrever no terminal.
+@param Symbol o character que deve ser impresso na tela.
+*/
+displayPoint(Point, Symbol) :-
+    createPoint(X, Y, Point),
+    writeAt(X, Y, Symbol).
+
+/*
+escreve um símbolo (Symboll) em todas as coordenadas contidas em uma lista de pontos (Points).
+
+@predicate displayPoints(+Points, +Symbol).
+
+@param Points uma lista de pontos que contêm as coordenadas X's e Y's para escrever no terminal.
+@param Symbol o character que deve ser impresso na tela.
+*/
+displayPoints([], _).
+displayPoints(Points, Symbol) :-
+    [P|Ps] = Points,
+    displayPoint(P, Symbol),
+    displayPoints(Ps, Symbol).
+
+/*
+escreve o tabuleiro no terminal.
+
+@predicate displayBoard(+Board).
+
+@param Board o tabuleiro que deve ser impresso no terminal.
+*/
+displayBoard(Board) :-
+    clearDisplay,
+    displayPoints(Board.walls, '#'),
+    displayPoint(Board.player, '@').
+
---
+++ src/utils.pl
@@ -0,0 +1,30 @@
+:- module(utils, [createPoint/3]).
+
+/*
+Cria um ponto que tem a coordenada X e Y.
+
+@predicate createPoint(+X, +Y, -Point).
+
+@param X a coordenada X do ponto.
+@param Y a coordenada Y do ponto.
+
+@return Sucede se a geração de um ponto com coordenadas X e Y forem válidas,
+        Falha caso contrário.
+
+@example
+    createPoint(10, 8, Point).
+    Point = 10-8.
+
+    createPoint(X, Y, 10-8).
+    X = 10,
+    Y = 8.
+
+    createPoint(X, _, 10-8).
+    X = 10.
+*/
+createPoint(X, Y, Point) :-
+    Point = X-Y.
+
+
+wall_symbol('#').
+player_symbol('@').
