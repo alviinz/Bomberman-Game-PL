2291724801681b96c97ae7916a7bb828824235f9
Matheus-Adiel <matheuus.adiiel@gmail.com>
2025-09-13 17:12:52 -0300

    features: connect new funcionts to personlize terminal with board game

--- src/display_board.pl
+++ src/display_board.pl
@@ -18,6 +18,19 @@ displayPoint(Point, Symbol) :-
     writeAt(X, Y, Symbol).
 
 /*
+Faz a mesma coisa que o displayPoint/2, com a adição de personalizar o símbolo.
+
+@predicate displayPoint(+Point, +Symbol, +Codes).
+
+@param Point  o ponto que contém as coordenadas X e Y para escrever no terminal.
+@param Symbol o character que deve ser impresso na tela.
+@param Codes  os códigos para a personalização do caractere no formato ANSI. (veja a documentação de persnonalizeTerm para a consulta dos códigos disponíveis)
+*/
+displayPoint(Point, Symbol, Codes) :-
+    createPoint(X, Y, Point),
+    writeAt(X, Y, Symbol, Codes).
+
+/*
 escreve um símbolo (Symboll) em todas as coordenadas contidas em uma lista de pontos (Points).
 
 @predicate displayPoints(+Points, +Symbol).
@@ -31,6 +44,20 @@ displayPoints(Points, Symbol) :-
     displayPoint(P, Symbol),
     displayPoints(Ps, Symbol).
 
+/*
+possue o mesmo funcionamento do displayPoints/2, mas com a adição de personalização do símbolo.
+
+@predicate displayPoints(+Points, +Symbol, +Codes).
+
+@param Points uma lista de pontos que contêm as coordenadas X's e Y's para escrever no terminal.
+@param Symbol o character que deve ser impresso na tela.
+@param Codes  os códicos de personalização do caractere no formato ANSI. (veja a documentação de personalizeTerm para a consulta dos códigos de cores disponíveis).
+*/
+displayPoints([], _, _).
+displayPoints(Points, Symbol, Color) :-
+    [P|Ps] = Points,
+    displayPoint(P, Symbol, Color),
+    displayPoints(Ps, Symbol, Color).
 
 /*
 escreve o tabuleiro no terminal.
@@ -41,10 +68,15 @@ escreve o tabuleiro no terminal.
 */
 displayBoard(Board) :-
     clearDisplay,
-    displayPoints(Board.walls, '#'),
-    displayPoints(Board.boxes, '='),
-    displayPoint(Board.player, 'P'),
+    wall_symbol(Ws), player_symbol(Ps), bomb_symbol(Bs), box_symbol(Boxs), 
     Bombs = Board.bombs,
     maplist(get_bomb_position, Bombs, BombPositions),
-    displayPoints(BombPositions, 'o').
+    displayPoints(Board.walls, Ws), 
+    displayPoints(Board.boxes, Boxs, '30;103'), 
+    displayPoints(BombPositions, Bs, '91;5'),
+    displayPoint(Board.player, Ps).
 
+wall_symbol('█').
+player_symbol('𖦔').
+bomb_symbol('δ').
+box_symbol('▓').
