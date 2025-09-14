2eddbce2e2261919704a3566c2bc3986ac121fb4
Matheus-Adiel <matheuus.adiiel@gmail.com>
2025-09-14 19:25:17 -0300

    docs: standardize docs

--- src/display_board.pl
+++ src/display_board.pl
@@ -6,37 +6,37 @@
 :- use_module(utils).
 
 /*
-escreve em uma coordenada da tela (Point) um símbolo (Symboll).
-
-@predicate displayPoint(+Point, +Symbol).
-
-@param Point  o ponto que contém as coordenadas X e Y para escrever no terminal.
-@param Symbol o character que deve ser impresso na tela.
+ * escreve em uma coordenada da tela (Point) um símbolo (Symboll).
+ * 
+ * @predicate displayPoint(+Point, +Symbol).
+ * 
+ * @param Point  o ponto que contém as coordenadas X e Y para escrever no terminal.
+ * @param Symbol o character que deve ser impresso na tela.
 */
 displayPoint(Point, Symbol) :-
     createPoint(X, Y, Point),
     writeAt(X, Y, Symbol).
 
 /*
-Faz a mesma coisa que o displayPoint/2, com a adição de personalizar o símbolo.
-
-@predicate displayPoint(+Point, +Symbol, +Codes).
-
-@param Point  o ponto que contém as coordenadas X e Y para escrever no terminal.
-@param Symbol o character que deve ser impresso na tela.
-@param Codes  os códigos para a personalização do caractere no formato ANSI. (veja a documentação de persnonalizeTerm para a consulta dos códigos disponíveis)
+ * faz a mesma coisa que o displayPoint/2, com a adição de personalizar o símbolo.
+ * 
+ * @predicate displayPoint(+Point, +Symbol, +Codes).
+ * 
+ * @param Point  o ponto que contém as coordenadas X e Y para escrever no terminal.
+ * @param Symbol o character que deve ser impresso na tela.
+ * @param Codes  os códigos para a personalização do caractere no formato ANSI. (veja a documentação de persnonalizeTerm para a consulta dos códigos disponíveis)
 */
 displayPoint(Point, Symbol, Codes) :-
     createPoint(X, Y, Point),
     writeAt(X, Y, Symbol, Codes).
 
 /*
-escreve um símbolo (Symboll) em todas as coordenadas contidas em uma lista de pontos (Points).
-
-@predicate displayPoints(+Points, +Symbol).
-
-@param Points uma lista de pontos que contêm as coordenadas X's e Y's para escrever no terminal.
-@param Symbol o character que deve ser impresso na tela.
+ * escreve um símbolo (Symboll) em todas as coordenadas contidas em uma lista de pontos (Points).
+ * 
+ * @predicate displayPoints(+Points, +Symbol).
+ * 
+ * @param Points uma lista de pontos que contêm as coordenadas X's e Y's para escrever no terminal.
+ * @param Symbol o character que deve ser impresso na tela.
 */
 displayPoints([], _).
 displayPoints(Points, Symbol) :-
@@ -45,13 +45,13 @@ displayPoints(Points, Symbol) :-
     displayPoints(Ps, Symbol).
 
 /*
-possue o mesmo funcionamento do displayPoints/2, mas com a adição de personalização do símbolo.
-
-@predicate displayPoints(+Points, +Symbol, +Codes).
-
-@param Points uma lista de pontos que contêm as coordenadas X's e Y's para escrever no terminal.
-@param Symbol o character que deve ser impresso na tela.
-@param Codes  os códicos de personalização do caractere no formato ANSI. (veja a documentação de personalizeTerm para a consulta dos códigos de cores disponíveis).
+ * possue o mesmo funcionamento do displayPoints/2, mas com a adição de personalização do símbolo.
+ * 
+ * @predicate displayPoints(+Points, +Symbol, +Codes).
+ * 
+ * @param Points uma lista de pontos que contêm as coordenadas X's e Y's para escrever no terminal.
+ * @param Symbol o character que deve ser impresso na tela.
+ * @param Codes  os códicos de personalização do caractere no formato ANSI. (veja a documentação de personalizeTerm para a consulta dos códigos de cores disponíveis).
 */
 displayPoints([], _, _).
 displayPoints(Points, Symbol, Color) :-
@@ -60,11 +60,11 @@ displayPoints(Points, Symbol, Color) :-
     displayPoints(Ps, Symbol, Color).
 
 /*
-escreve o tabuleiro no terminal.
-
-@predicate displayBoard(+Board).
-
-@param Board o tabuleiro que deve ser impresso no terminal.
+ * escreve o tabuleiro no terminal.
+ * 
+ * @predicate displayBoard(+Board).
+ * 
+ * @param Board o tabuleiro que deve ser impresso no terminal.
 */
 displayBoard(Board) :-
     clearDisplay,
@@ -81,6 +81,10 @@ displayBoard(Board) :-
     displayPoints(AllExplosionPoints,'x'),
     displayPoint(Board.player, Ps).
 
+
+/*
+ * mapeamento dos símbolos de cada elemento do tabuleiro.
+*/
 wall_symbol('█').
 player_symbol('𖦔').
 bomb_symbol('δ').
