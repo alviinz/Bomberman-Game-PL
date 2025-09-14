9054f9e198d677341884cffa2ca5b9d40afbfcc6
alviinz <planetadosjogos2006@gmail.com>
2025-09-14 18:01:07 -0300

    fixed explosion area and bomb timer and tick

--- src/display_common.pl
+++ src/display_common.pl
@@ -6,3 +6,18 @@ exitDisplay(Configs) :-
     writeAt(0, 1, 'bye, bye. See you later!'),
     movePointer(0, 3),
     !.
+
+
+/*
+Mostra o ecrã de Game Over.
+
+@predicate display_game_over(+Configs).
+*/
+display_game_over(Configs) :-
+    % Pode personalizar as coordenadas e a mensagem como quiser
+    get_dict(width, Configs, W),
+    get_dict(height, Configs, H),
+    MsgX is W // 2 - 5,
+    MsgY is H // 2,
+    writeAt(MsgX, MsgY, "GAME OVER!", [red]),
+    movePointer(0, H + 2). % Move o cursor para baixo do tabuleiro
